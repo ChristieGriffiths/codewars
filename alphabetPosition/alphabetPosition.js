33a6432446d15ff3ebaf1cf638ca5884f1ecb312
Christie Griffiths <115458394+ChristieGriffiths@users.noreply.github.com>
2023-07-09 13:50:44 +0100

    pass 3 tests

--- alphabetPosition/alphabetPosition.js
+++ alphabetPosition/alphabetPosition.js
@@ -1,5 +1,14 @@
 function alphabetPosition(text) {
-  return '1';
+  splitText = text.split("")
+  result = []
+  pattern = /[a-zA-Z]/
+  for(i = 0; i < splitText.length; i++) {
+    let element = splitText[i]
+    if(pattern.test(element)) {
+      result.push(element.charCodeAt(0) - 96)
+    }
+  }
+  return result.join(' ')
 }
 
 module.exports = alphabetPosition;
--- alphabetPosition/alphabetPosition.test.js
+++ alphabetPosition/alphabetPosition.test.js
@@ -3,6 +3,11 @@ const alphabetPositon = require ('./alphabetPosition');
 describe('', () => {
   it('returns a 1 for the letter a', () => {
     expect(alphabetPositon('a')).toEqual('1')
-
+  })
+  it('returns a 1 for the letter a and 2 for b', () => {
+    expect(alphabetPositon('ab')).toEqual('1 2')
+  })
+  it('returns a 1 for the letter a and 2 for b but nothing if it\'s not a letter', () => {
+    expect(alphabetPositon('ab3')).toEqual('1 2')
   })
 })
