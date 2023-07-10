fa23845380dea1fc880940481e0add3f1cee9a21
Christie Griffiths <115458394+ChristieGriffiths@users.noreply.github.com>
2023-07-10 09:16:19 +0100

    pass 2 tests

--- binaryAddition/binaryAddition.js
+++ binaryAddition/binaryAddition.js
@@ -1,5 +1,15 @@
 function addBinary(a,b) {
-  return "10"
+  let inputSum = a + b 
+  let quotient = inputSum
+  let binaryResult = []
+  do {
+    binaryResult.push(quotient % 2);
+    // adds remainder to array
+    quotient = Math.floor(quotient / 2);
+    // resets the quotient to equal itself divided by 2 and rounded down
+  } while(quotient !== 0)
+  return binaryResult.reverse().join("");
+
 }
 
 module.exports = addBinary;
--- binaryAddition/binaryAddition.test.js
+++ binaryAddition/binaryAddition.test.js
@@ -4,4 +4,7 @@ describe('addBinary', () => {
   it('takes two numbers and returns their sum in binary', () => {
   expect(addBinary(1, 1)).toEqual('10')
   })
+  it('takes two numbers and returns their sum in binary', () => {
+  expect(addBinary(1, 0)).toEqual('1')
+  })
 })
