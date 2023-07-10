ece1b4059baf0b257b9e5f718dcaf807c5e8280c
Christie Griffiths <115458394+ChristieGriffiths@users.noreply.github.com>
2023-07-10 09:21:32 +0100

    refactor and complete kata

--- binaryAddition/binaryAddition.js
+++ binaryAddition/binaryAddition.js
@@ -1,15 +1,12 @@
 function addBinary(a,b) {
-  let inputSum = a + b 
-  let quotient = inputSum
+  let sum = a + b 
   let binaryResult = []
   do {
-    binaryResult.push(quotient % 2);
-    // adds remainder to array
-    quotient = Math.floor(quotient / 2);
-    // resets the quotient to equal itself divided by 2 and rounded down
-  } while(quotient !== 0)
-  return binaryResult.reverse().join("");
+    binaryResult.unshift(sum % 2);
+    sum = Math.floor(sum / 2);
+  } while(sum !== 0)
 
+  return binaryResult.join("");
 }
 
 module.exports = addBinary;
--- binaryAddition/binaryAddition.test.js
+++ binaryAddition/binaryAddition.test.js
@@ -10,4 +10,7 @@ describe('addBinary', () => {
   it('takes two numbers and returns their sum in binary', () => {
   expect(addBinary(1, 1)).toEqual('10')
   })
+  it('takes two numbers and returns their sum in binary', () => {
+  expect(addBinary(5, 9)).toEqual('1110')
+  })
 })
