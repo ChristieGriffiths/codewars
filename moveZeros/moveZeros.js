1291882e02d9d74928fae1d7fcdabae35a53e04a
Christie Griffiths <115458394+ChristieGriffiths@users.noreply.github.com>
2023-07-11 09:00:48 +0100

    pass 2 tests

--- moveZeros/moveZeros.js
+++ moveZeros/moveZeros.js
@@ -1,4 +1,12 @@
 function moveZeros(arr) {
+  let zeros = []
+  for (let i = 0; i < arr.length; i ++) {
+    if(arr[i] === 0) {
+      zeros.push(0)
+      arr.splice(i, 1)
+    }
+  }
+  arr.push(...zeros)
   return arr
 }
 
--- moveZeros/moveZeros.test.js
+++ moveZeros/moveZeros.test.js
@@ -4,4 +4,10 @@ describe('moveZeros', () => {
   it('doesn\'t remove non-zeros', () => {
     expect(moveZeros([1])).toEqual([1])
   });
+  it('removes 0s and places them at end of array', () => {
+    expect(moveZeros([0, 1])).toEqual([1, 0])
+  });
+  it('removes 0s and places them at end of array', () => {
+    expect(moveZeros([1,2,0,1,0,1,0,3,0,1])).toEqual([1, 2, 1, 1, 3, 1, 0, 0, 0, 0])
+  });
 });
