6e0f368259faaefa1d778f7e13dd4570f6824e91
Christie Griffiths <115458394+ChristieGriffiths@users.noreply.github.com>
2023-07-17 16:45:54 +0100

    re-attempted

--- snail/snail.js
+++ snail/snail.js
@@ -1,17 +1,43 @@
 snail = function(array) {
-  let result = array[0]
-  if(array.length !== 1) {
-    for(let i = 1; i < array.length - 1; i++) {
+  result = []
+
+  function sumNestedArrays(arrays) {
+    let total = 0;
+  
+    for (let i = 0; i < arrays.length; i++) {
+      for (let j = 0; j < arrays[i].length; j++) {
+        total += arrays[i][j];
+      }
+    }
+    return total;
+  }
+
+  while (sumNestedArrays(array) !== 0) {
+    result.push(array[0])
+    // add the 0th array to the the result
+    array.splice(0, 1);
+    // delete the 0th array from the arrays
+    for(let i = 0; i < array.length - 1; i++) {
       let lastElement = array[i].length - 1
       result.push(array[i][lastElement])
+      // add the last element of every array the result
       array[i].splice(lastElement, 1)  
+      // delete the last element of every array
     }
-    
-  } else {
-    return array[0];
+    let lastElements = array[array.length - 1]
+    result.push(lastElements);
+    // push the last array to the result but in reverse order 
+    result.splice(array[array.length - 1, 1]);
+    // delete the last array from the arrays
+    for(let i = array.length - 0; i === 0; i--) {
+      result.push(array[i][0]);
+      // add the first element of each array but in reverse order
+      result.splice(array[i][0], 1)
+      // remove the first element after adding it
+    }
   }
-  console.log(result)
-  console.log(array)
+  console.log(result);
+  return(result);
 }
 
 module.exports = snail;
--- snail/snail.test.js
+++ snail/snail.test.js
@@ -2,19 +2,19 @@ const snail = require ("./snail")
 
 describe("snail", () => {
   it("takes an array of an empty array and returns the empty array", () => {
-    input = [[]]
+    const input = [[]]
     expect(snail(input)).toEqual([])
   })
   it("takes an array of a non-empty array and returns the inside array", () => {
-    input = [[1]]
+    const input = [[1]]
     expect(snail(input)).toEqual([1])
   })
   it("takes array of arrays and returns an array of the input but in a snake like fashion", () => {
-    input = [[1, 2], [3, 4]]
+    const input = [[1, 2], [3, 4]]
     expect(snail(input)).toEqual([1, 2, 4, 3])
   })
   it("takes array of arrays and returns an array of the input but in a snake like fashion", () => {
-    input = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    const input = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     expect(snail(input)).toEqual([1, 2, 3, 6, 9, 8, 7, 4, 5])
   })
 })
