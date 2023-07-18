7f249358b28e41f491ccdeafccd4de8a273238a5
Christie Griffiths <christiegriffiths@outlook.com>
2023-07-18 10:56:58 +0100

    slow but steady

---
+++ snail/play.js
@@ -0,0 +1,15 @@
+let array = [10, 20, 30]
+
+let result = []
+
+console.log(array.indexOf(0))
+console.log(array.indexOf(1))
+console.log(array.indexOf(2
+  
+  ))
+console.log(array.indexOf(3))
+
+// result.push(array[1]);
+
+// console.log(array);
+// console.log(result);
--- snail/snail.js
+++ snail/snail.js
@@ -12,31 +12,43 @@ snail = function(array) {
     return total;
   }
 
-  while (sumNestedArrays(array) !== 0) {
-    result.push(array[0])
+  console.log(sumNestedArrays([[1, 2, 3], [4, 5, 6], [7, 8, 9]]));
+
+  while (sumNestedArrays(array) > 0) {
+    console.log('I made it into the loop')
+    result.push(...array[0])
+    console.log(result)
     // add the 0th array to the the result
     array.splice(0, 1);
+    console.log(array);
     // delete the 0th array from the arrays
-    for(let i = 0; i < array.length - 1; i++) {
+    for(let i = 0; i <= array.length - 1 ; i++) {
       let lastElement = array[i].length - 1
       result.push(array[i][lastElement])
+      console.log(`the result when adding the last element of every array: loop: ${i}  : ${result}`)
       // add the last element of every array the result
-      array[i].splice(lastElement, 1)  
+      array[i].splice(lastElement, 1) 
+      console.log(`the array when removing the last element of every array: loop: ${i}  : ${array}`)
       // delete the last element of every array
     }
+    console.log('makes it out of the for loop')
+    console.log('result after the for loop: ' + result);
+
     let lastElements = array[array.length - 1]
-    result.push(lastElements);
+    lastElements === undefined ? lastElements.reverse() : console.log("code ran this far");
+    console.log(result)
+    console.log(array.length - 1)
     // push the last array to the result but in reverse order 
-    result.splice(array[array.length - 1, 1]);
+    array.splice(array.length - 1, 1);
     // delete the last array from the arrays
+    console.log('array at line 44 : ' + array)
     for(let i = array.length - 0; i === 0; i--) {
-      result.push(array[i][0]);
+      array[i][0] === undefined ? result.push(array[i][0]) : console.log("code ran this far");
       // add the first element of each array but in reverse order
       result.splice(array[i][0], 1)
       // remove the first element after adding it
     }
   }
-  console.log(result);
   return(result);
 }
 
