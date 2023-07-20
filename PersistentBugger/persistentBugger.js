9efbae41d29eb113250488b0e861e58ccc0c0d5b
Christie Griffiths <115458394+ChristieGriffiths@users.noreply.github.com>
2023-07-20 09:37:19 +0100

    pass tests

--- PersistentBugger/persistentBugger.js
+++ PersistentBugger/persistentBugger.js
@@ -2,8 +2,9 @@ function persistence(num) {
   let result = num;
   let counter = 0;
   while(result.toString().length > 1) {
-    let splitInput = result.toString().split("")
+    let splitInput = result.toString().split("");
     result = splitInput.reduce((accumulator, currentValue) => accumulator * currentValue);
+    counter += 1;
   }
   return counter; 
 }
