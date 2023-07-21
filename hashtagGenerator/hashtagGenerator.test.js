d4d5e88c56c4031656376ec98229f9f11e562bfa
Christie Griffiths <115458394+ChristieGriffiths@users.noreply.github.com>
2023-07-21 09:37:34 +0100

    pass 1 test

---
+++ hashtagGenerator/hashtagGenerator.test.js
@@ -0,0 +1,8 @@
+const generateHashtag = require('./hashtagGenerator')
+
+describe('uniqueInOrder', () => {
+  it('takes an empty string and returns false', () =>{
+    const input = ""
+    expect(generateHashtag(input)).toEqual(false)
+  });
+});
