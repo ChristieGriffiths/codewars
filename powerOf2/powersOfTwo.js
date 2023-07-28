74320ace1e9c7658d05e20d9f908851e3ed6cdca
Christie Griffiths <115458394+ChristieGriffiths@users.noreply.github.com>
2023-07-28 08:47:20 +0100

    pass two tests

--- powerOf2/powersOfTwo.js
+++ powerOf2/powersOfTwo.js
@@ -1,5 +1,14 @@
 function powersOfTwo(n){
-  return [1]
+  let result = [];
+  for (let i = 0; i <= n; i++) {
+    console.log('makes it into loop');
+    console.log('i equals', i);
+    console.log( 2** i)
+    result.push(2 ** i);
+  }
+  console.log('result after loop', result);
+
+  return result;
 }
 
 module.exports = powersOfTwo;
--- powerOf2/powersOfTwo.test.js
+++ powerOf2/powersOfTwo.test.js
@@ -2,6 +2,9 @@ const powersOfTwo = require('./powersOfTwo')
 
 describe('powersOfTwo', () => {
   it('returns [1] for an input of 0 ', () => {
-    expect(powersOfTwo(1)).toEqual([1])
+    expect(powersOfTwo(0)).toEqual([1])
+  })
+  it('returns [1, 2] for an input of 0 ', () => {
+    expect(powersOfTwo(1)).toEqual([1, 2])
   })
 })
