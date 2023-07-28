c6fd6826f30afa74765f386ffb74d8d6e239919b
Christie Griffiths <115458394+ChristieGriffiths@users.noreply.github.com>
2023-07-28 08:49:02 +0100

    remove console.logs and pass kata

--- powerOf2/powersOfTwo.js
+++ powerOf2/powersOfTwo.js
@@ -1,13 +1,9 @@
 function powersOfTwo(n){
   let result = [];
   for (let i = 0; i <= n; i++) {
-    console.log('makes it into loop');
-    console.log('i equals', i);
     console.log( 2** i)
     result.push(2 ** i);
   }
-  console.log('result after loop', result);
-
   return result;
 }
 
