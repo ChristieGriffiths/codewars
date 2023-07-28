133bf389604d267f6b22430971281df35e519d2d
Christie Griffiths <115458394+ChristieGriffiths@users.noreply.github.com>
2023-07-28 08:48:23 +0100

    pass 3 tests

--- powerOf2/powersOfTwo.test.js
+++ powerOf2/powersOfTwo.test.js
@@ -7,4 +7,7 @@ describe('powersOfTwo', () => {
   it('returns [1, 2] for an input of 0 ', () => {
     expect(powersOfTwo(1)).toEqual([1, 2])
   })
+  it('returns correct powers of two for input of 4', () => {
+    expect(powersOfTwo(4)).toEqual([1, 2, 4, 8, 16])
+  })
 })
