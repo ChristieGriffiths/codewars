e513a75f14c852fb14d59cc3719e7cedef1d75cc
Christie Griffiths <115458394+ChristieGriffiths@users.noreply.github.com>
2023-07-31 10:39:43 +0100

    pass 3 tests

--- isograms/isIsogram.js
+++ isograms/isIsogram.js
@@ -1,5 +1,18 @@
 function isIsogram(str){
-  return true
+  const characterCount = {}
+  
+  str.split('').forEach(function(letter) {
+    if(characterCount[letter]===undefined) {
+      characterCount[letter] = 1
+    } else {
+      characterCount[letter] += 1
+    }
+  })
+  for (const key in characterCount) {
+    const value = characterCount[key];
+    if(value > 1) { return false }
+    return true 
+  }
 }
 
 module.exports = isIsogram;
--- isograms/isIsogram.test.js
+++ isograms/isIsogram.test.js
@@ -4,4 +4,10 @@ describe("isIsogram", () => {
   it("returns true when no characters repeat", () => {
     expect(isIsogram('a')).toEqual(true)
   })
+   it("returns false when characters are repeated", () => {
+    expect(isIsogram('aa')).toEqual(false)
+  })
+   it("returns true when no characters are repeated", () => {
+    expect(isIsogram('Dermatoglyphics')).toEqual(true)
+  })
 })
