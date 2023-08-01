7b9152ae78e0e27b8dff17a53cbefd2f13538f4f
Christie Griffiths <115458394+ChristieGriffiths@users.noreply.github.com>
2023-08-01 14:14:52 +0100

    pass kata

--- declareWinner/declareWinner.js
+++ declareWinner/declareWinner.js
@@ -1,5 +1,19 @@
 function declareWinner(fighter1, fighter2, firstAttacker) {
-  return "Write your code here";
+  let attacker;
+  let defender;
+  if(firstAttacker === fighter1.name) {
+    attacker = fighter1
+    defender = fighter2
+  } else {
+    attacker = fighter2
+    defender = fighter1
+  }
+  while (attacker.health > 0 && defender.health > 0) {
+    defender.health -= attacker.damagePerAttack
+    let temp = attacker;
+    attacker = defender;
+    defender = temp;
+  }
+  return defender.name
 }
 
-module.exports = declareWinner;
