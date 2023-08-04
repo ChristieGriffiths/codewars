f87ffa966541e127c964164d558dab7a3cff57f2
Christie Griffiths <115458394+ChristieGriffiths@users.noreply.github.com>
2023-08-04 08:54:09 +0100

    re-factor kata

--- isograms/isIsogram.js
+++ isograms/isIsogram.js
@@ -1,16 +1,14 @@
 function isIsogram(str) {
-  characterCount = {};
-  const lowerCaseInput = str.toLowerCase()
-  for(const char of lowerCaseInput) {
-    if(characterCount[char] === undefined) {
-      characterCount[char] = 1
-    } else {
-      characterCount[char] += 1
+  const lowerCaseInput = str.toLowerCase();
+  const characterCount = {};
+  
+  for (const char of lowerCaseInput) {
+    if (characterCount[char]) {
+      return false;
     }
+    characterCount[char] = true;
   }
-  for (const char in characterCount) {
-    if(characterCount[char] > 1) { return false } 
-  }
+  
   return true;
 }
 
